c7449acc04e2dbca6ed6db038cf4ff2163457026
yuanhong <yuanhong@sina.com>
2019-07-24 15:43:27 +0800

    未完成

--- src/pages/Found/Found.vue
+++ src/pages/Found/Found.vue
@@ -15,79 +15,74 @@
         </ul>
       </div>
     </div>
-    <div class="allList">
-      <div class="wrap" v-for="(items,index) in found1" :key="index">
-        <div class="content"  v-for="item in items.topics" :key="item.name">
-          <div class="li"  v-if="item.style===1">
-            <div class="firstList">
-              <div class="info-name">
-                <img :src="item.avatar" alt />
-                <span>{{item.nickname}}</span>
-              </div>
-              <div class="title">{{item.title}}</div>
-              <div class="img">
-                <img
-                  :src="item.picUrl"
-                   width="100%"
-                        height="100%"
-                  alt
-                />
-              </div>
-              <div class="views">
-                <i class="iconfont icon-weibiaoti2fuzhi15"></i>
-                <span>4027人看过</span>
+    <div class="allListWrap">
+      <div class="allList">
+        <div class="wrap" v-for="(items,index) in found1" :key="index">
+          <div class="content" v-for="item in items.topics" :key="item.name">
+            <div class="li" v-if="item.style===1">
+              <div class="firstList">
+                <div class="info-name">
+                  <img :src="item.avatar" alt />
+                  <span>{{item.nickname}}</span>
+                </div>
+                <div class="title">{{item.title}}</div>
+                <div class="img">
+                  <img :src="item.picUrl" width="100%" height="100%" alt />
+                </div>
+                <div class="views">
+                  <i class="iconfont icon-weibiaoti2fuzhi15"></i>
+                  <span>4027人看过</span>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="li"  v-if="item.style===2 ">
-            <div class="m-tpls m-tpls-picker">
-              <a href="javascript:;" class="u-flexbox">
-                <div class="info">
-                  <div class="u-name">
-                    <span class="ava">
-                      <img
-                        src="https://yanxuan.nosdn.127.net/8720248cc1f62d279f004b8a5124f811.jpg?imageView&amp;quality=65&amp;thumbnail=56y56"
-                        alt
-                        width="100%"
-                        height="100%"
-                      />
-                    </span>
-                    <span>{{item.nickname}}</span>
+            <div class="li" v-if="item.style===2 ">
+              <div class="m-tpls m-tpls-picker">
+                <a href="javascript:;" class="u-flexbox">
+                  <div class="info">
+                    <div class="u-name">
+                      <span class="ava">
+                        <img
+                          src="https://yanxuan.nosdn.127.net/8720248cc1f62d279f004b8a5124f811.jpg?imageView&amp;quality=65&amp;thumbnail=56y56"
+                          alt
+                          width="100%"
+                          height="100%"
+                        />
+                      </span>
+                      <span>{{item.nickname}}</span>
+                    </div>
+                    <div class="title">{{item.title}}</div>
+                    <div class="desc">{{item.desc}}</div>
+                    <div class="u-rcount">
+                      <i class="iconfont icon-weibiaoti2fuzhi15"></i>
+                      <span>7937人看过</span>
+                    </div>
                   </div>
-                  <div class="title">{{item.title}}</div>
-                  <div class="desc">{{item.desc}}</div>
-                  <div class="u-rcount">
-                    <i class="iconfont icon-weibiaoti2fuzhi15"></i>
-                    <span>7937人看过</span>
+                  <div class="u-pic">
+                    <img :src="item.picUrl" alt width="100%" height="100%" />
                   </div>
-                </div>
-                <div class="u-pic">
-                  <img
-                   :src="item.picUrl"
-                    alt
-                    width="100%"
-                    height="100%"
-                  />
-                </div>
-              </a>
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    <img src="" alt="">
+    <img src alt />
   </div>
 </template>
 <script>
-import BScroll from 'better-scroll'
+import BScroll from "better-scroll";
 import { reqFound } from "../../api";
 import { mapState } from "vuex";
+import { watch } from "fs";
 export default {
   data() {
     return {
       foundList: [],
       foundList1: [],
-      current: ""
+      current: "",
+      recommendList: [], //存放滚动区域要显示的数据
+      isLoading: false //默认没有加载数据
     };
   },
   computed: {
@@ -97,37 +92,76 @@ export default {
     headerNav(index) {
       console.log(111);
       this.current = index;
+    },
+    initScroll() {
+      //视口的高度
+      const viewHeight = document.documentElement.clientHeight;
+      console.log(viewHeight);
+      //固定的高  包裹器
+      const scrollTop1 = document.querySelector(".allListWrap");
+      console.log(scrollTop1);
+      //ul的高
+      const allListHeight = document.querySelector(".allList");
+      console.log(allListHeight);
+      // const scrollY =  this.scrollY = allListHeight.scrollHeight-scrollTop1.scrollHeight
+      // console.log(scrollY,'滑动的距离' )
+      // scrollTop1.style.height = viewHeight+'px'
+
+      this.$nextTick(() => {
+        this.allListWrap = new BScroll(".allListWrap", {
+          // click: true,
+          scrollY: true,
+          probeType: 3,
+          // 忽略竖直方向的滚动
+          // scrollY: false,
+          // eventPassthrough: "vertical"
+        });
+
+        this.allListWrap.on("scroll", ({y}) => {
+          // console.log('2222',y);
+          //滑动过程中的位置
+          this.scrollY = Math.abs(y);
+        });
+        this.allListWrap.on("scrollEnd", ({ y }) => {
+          console.log("滑动ul最后停的位置", y);
+          //滑动ul最后停的位置
+          this.scrollY = Math.abs(y);
+             
+        });
+         this.allListWrap.on("touchEnd", ({ y }) => {
+          console.log('手指或者鼠标离开的位置',  y);
+          //手指或者鼠标离开的位置
+          this.scrollY = Math.abs(y);
+              
+        });
+        new BScroll(".cateListNav", {
+          click: true,
+          scrollX: true
+          // 忽略竖直方向的滚动
+          // scrollY: false,
+          // eventPassthrough: "vertical"
+        });
+      });
     }
   },
   mounted() {
-    const viewHeight = document.documentElement.clientHeight
-    console.log(viewHeight);
-    
+    this.$nextTick(() => {
+        // 页面显示完成后设置将要实现scroll滚动效果的元素的父元素的高度，使子元素高度超出父元素才能实现滚动
+      //视口的高度
+      const viewHeight = document.documentElement.clientHeight;
+      console.log(viewHeight);
+      //固定的高  包裹器
+      const scrollTop1 = document.querySelector(".allListWrap");
+      console.log(scrollTop1);
+        scrollTop1.style.height = viewHeight + 'px';
+        // 实现滚动
+        this.initScroll();
+      })
+    this.initScroll();
     this.$store.dispatch("getFound");
     this.$store.dispatch("getFound1");
-    // this.foundList = result.data
-    // const result =await reqFound()
-    // console.log('result',result.data.data.data)
-    // // console.log(result.categoryGroupList)
-    // this.foundList= result.data.data.data
-    // this.foundList1= result.data.data.data1.topics
-    // console.log('result',result.data.data.data1[0].topics)
-    new BScroll(".cateListNav", {
-      click: true,
-      scrollX: true
-      // 忽略竖直方向的滚动
-      // scrollY: false,
-      // eventPassthrough: "vertical"
-    });
-    //  new BScroll(".content", {
-    //   click: true,
-    //   // scrollX: true
-    //   // 忽略竖直方向的滚动
-    //   scrollY: true,
-    //   // eventPassthrough: "vertical"
-    // })
   },
- 
+  watch: {}
 };
 </script>
 <style lang="stylus" rel="stylesheet/stylus" scoped>
@@ -226,187 +260,191 @@ export default {
     }
   }
 
-  .allList {
-    background: #f4f4f4;
+  .allListWrap {
+    height: 1000px;
     position: relative;
     top: 2rem;
     left: 0;
 
-    .wrap {
-      // height 920px
-      padding: 0.3rem 0 0.2rem 0;
+    .allList {
+      background: #f4f4f4;
 
-      .content {
-        padding: 0.36rem 0.3rem;
-        height: 100%;
-        width: 100%;
-        background: #fff;
-        margin: 0.2rem 0;
-        box-sizing: border-box;
+      .wrap {
+        // height 920px
+        padding: 0.3rem 0 0.2rem 0;
 
-        .li{
-          border-bottom: 10px solid #f4f4f4;
+        .content {
+          padding: 0.36rem 0.3rem;
           height: 100%;
           width: 100%;
+          background: #fff;
+          margin: 0.2rem 0;
+          box-sizing: border-box;
 
-          .firstList {
-            .info-name {
-              display: flex;
-              font-size: 0.28rem;
-              color: #333;
-              line-height: 0.36rem;
-              white-space: nowrap;
-              overflow: hidden;
-              text-overflow: ellipsis;
-              margin-bottom: 0.24rem;
-
-              img {
-                width: 0.56rem;
-                height: 0.56rem;
-                overflow: hidden;
-                border-radius: 50%;
-                -webkit-box-sizing: border-box;
-                box-sizing: border-box;
-                border: 0.01rem solid #d9d9d9;
-                margin-right: 0.12rem;
-                vertical-align: middle;
-              }
-
-              span {
-                display: inline-block;
-                vertical-align: middle;
-                font-size: 24px;
-                vertical-align: middle;
-                line-height: 42px;
-              }
-            }
-
-            .title {
-              font-size: 35px;
-              color: #333;
-              line-height: 0.52rem;
-              margin: -0.08rem 0 0.16rem 0;
-              overflow: hidden;
-              text-overflow: ellipsis;
-              font-family: PingFangSC-Regular;
-            }
-
-            .img {
-              width: 100%;
-              // height: 3.76rem;
-              position: relative;
-              margin-bottom: 0.2rem;
-              overflow: hidden;
-              border-radius: 0.08rem;
-            }
-
-            .views {
-              color: #000;
-              line-height: 0.32rem;
-              margin-top: 0.18rem;
-              font-size: 26px;
-              font-weight: 700;
-              margin-bottom: 17px;
-
-              i {
-                font-size: 27px;
-                margin-right: 10px;
-                display: inline-block;
-              }
-            }
-          }
-
-          .m-tpls-picker {
-            position: relative;
-            padding: 0.32rem 0.3rem;
+          .li {
+            border-bottom: 10px solid #f4f4f4;
+            height: 100%;
             width: 100%;
-            background: #fff;
-            margin: 0.2rem 0;
-            box-sizing: border-box;
 
-            .u-flexbox {
-              display: flex;
-              flex-wrap: wrap;
-              justify-content: space-between;
+            .firstList {
+              .info-name {
+                display: flex;
+                font-size: 0.28rem;
+                color: #333;
+                line-height: 0.36rem;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                margin-bottom: 0.24rem;
 
-              .info {
-                width: 4rem;
-
-                .u-name {
-                  font-size: 0.28rem;
-                  color: #333;
-                  line-height: 0.36rem;
-                  white-space: nowrap;
+                img {
+                  width: 0.56rem;
+                  height: 0.56rem;
                   overflow: hidden;
-                  text-overflow: ellipsis;
-                  display: flex;
+                  border-radius: 50%;
+                  -webkit-box-sizing: border-box;
+                  box-sizing: border-box;
+                  border: 0.01rem solid #d9d9d9;
+                  margin-right: 0.12rem;
+                  vertical-align: middle;
+                }
 
-                  span {
-                    font-size: 28px;
-                    line-height: 42px;
-                  }
-
-                  .ava {
-                    width: 0.56rem;
-                    height: 0.56rem;
-                    overflow: hidden;
-                    border-radius: 50%;
-                    box-sizing: border-box;
-                    border: 0.01rem solid #d9d9d9;
-                    margin-right: 0.12rem;
-
-                    img {
-                      border-radius: 50%;
-                      height: 40px;
-                      width: 40px;
-                    }
-                  }
+                span {
+                  display: inline-block;
+                  vertical-align: middle;
+                  font-size: 24px;
+                  vertical-align: middle;
+                  line-height: 42px;
                 }
               }
 
-              .u-pic {
-                width: 2.72rem;
-                height: 2.72rem;
+              .title {
+                font-size: 35px;
+                color: #333;
+                line-height: 0.52rem;
+                margin: -0.08rem 0 0.16rem 0;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                font-family: PingFangSC-Regular;
+              }
+
+              .img {
+                width: 100%;
+                // height: 3.76rem;
                 position: relative;
+                margin-bottom: 0.2rem;
                 overflow: hidden;
                 border-radius: 0.08rem;
-                background: #f0f0f0;
+              }
+
+              .views {
+                color: #000;
+                line-height: 0.32rem;
+                margin-top: 0.18rem;
+                font-size: 26px;
+                font-weight: 700;
+                margin-bottom: 17px;
+
+                i {
+                  font-size: 27px;
+                  margin-right: 10px;
+                  display: inline-block;
+                }
               }
             }
 
-            .title {
+            .m-tpls-picker {
+              position: relative;
+              padding: 0.32rem 0.3rem;
               width: 100%;
-              font-size: 34px;
-              color: #333;
-              line-height: 0.44rem;
-              overflow: hidden;
-              text-overflow: ellipsis;
-              display: -webkit-box;
-              -webkit-line-clamp: 2;
-              -webkit-box-orient: vertical;
-              padding-top: 0.32rem;
-              font-family: PingFangSC-Regular;
-            }
+              background: #fff;
+              margin: 0.2rem 0;
+              box-sizing: border-box;
 
-            .desc {
-              width: 100%;
-              white-space: nowrap;
-              overflow: hidden;
-              text-overflow: ellipsis;
-              font-size: 28px;
-              line-height: 0.4rem;
-              padding-top: 0.08rem;
-              color: #7f7f7f;
-            }
+              .u-flexbox {
+                display: flex;
+                flex-wrap: wrap;
+                justify-content: space-between;
 
-            .u-rcount {
-              font-size: 28px;
-              color: #999;
-              line-height: 0.32rem;
-              margin-top: 0.18rem;
+                .info {
+                  width: 4rem;
 
-              i {
+                  .u-name {
+                    font-size: 0.28rem;
+                    color: #333;
+                    line-height: 0.36rem;
+                    white-space: nowrap;
+                    overflow: hidden;
+                    text-overflow: ellipsis;
+                    display: flex;
+
+                    span {
+                      font-size: 28px;
+                      line-height: 42px;
+                    }
+
+                    .ava {
+                      width: 0.56rem;
+                      height: 0.56rem;
+                      overflow: hidden;
+                      border-radius: 50%;
+                      box-sizing: border-box;
+                      border: 0.01rem solid #d9d9d9;
+                      margin-right: 0.12rem;
+
+                      img {
+                        border-radius: 50%;
+                        height: 40px;
+                        width: 40px;
+                      }
+                    }
+                  }
+                }
+
+                .u-pic {
+                  width: 2.72rem;
+                  height: 2.72rem;
+                  position: relative;
+                  overflow: hidden;
+                  border-radius: 0.08rem;
+                  background: #f0f0f0;
+                }
+              }
+
+              .title {
+                width: 100%;
+                font-size: 34px;
+                color: #333;
+                line-height: 0.44rem;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-line-clamp: 2;
+                -webkit-box-orient: vertical;
+                padding-top: 0.32rem;
+                font-family: PingFangSC-Regular;
+              }
+
+              .desc {
+                width: 100%;
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
                 font-size: 28px;
+                line-height: 0.4rem;
+                padding-top: 0.08rem;
+                color: #7f7f7f;
+              }
+
+              .u-rcount {
+                font-size: 28px;
+                color: #999;
+                line-height: 0.32rem;
+                margin-top: 0.18rem;
+
+                i {
+                  font-size: 28px;
+                }
               }
             }
           }
